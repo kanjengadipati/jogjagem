90788c9e459514f5477a84249826a3d0e4a0f022
Heriyadi <adipatikanjeng@gmail.com>
2026-08-18 07:44:07 +0700

    fix: allow /api/og-proxy in robots.txt to unblock GSC coverage

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: '*',
-        allow: '/',
+        allow: ['/api/og-proxy'],
         disallow: ['/api/', '/_next/', '/static/'],
       },
     ],
